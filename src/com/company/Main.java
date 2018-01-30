8b684c244a44080656afced113e0fd810964b598
Zigmas <zambrazaitis@gmail.com>
2018-01-30 15:57:57 +0200

    uzduotis10 finished

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,20 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // 1.10​. Sukurti skaičiuotuvo klasę, kurioje realizuoti statinius metodus sumai, skirtumui,
+        // sandaugai. Per parametrus bus naudojami sveiko tipo skaičiai.
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Iveskite pirmaji skaiciu: ");
+        int sk1 = sc.nextInt();
+        System.out.println("Iveskite antraji skaiciu: ");
+        int sk2 = sc.nextInt();
+        Veiksmai.suma(sk1, sk2);
+        Veiksmai.skirtumas(sk1, sk2);
+        Veiksmai.sandauga(sk1, sk2);
+
     }
 }
---
+++ src/com/company/Veiksmai.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Veiksmai {
+    public static void suma(int a, int b) {
+        int suma = a + b;
+        System.out.println("Suma= " + suma);
+    }
+
+    public static void skirtumas(int a, int b) {
+        int skirtumas = a - b;
+        System.out.println("Skirtumas= " + skirtumas);
+    }
+    public static void sandauga(int a, int b) {
+        int sandauga = a*b;
+        System.out.println("Sandauga= " + sandauga);
+    }
+}
